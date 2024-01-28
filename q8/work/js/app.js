70ae244f24948770423df2f5364075e55f70f46c
ogasawarayui <ogasawara.yui@outlook.com>
2024-01-29 02:50:46 +0900

    jq

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -4,10 +4,7 @@ $(function () {
   var pageCount = 1;
   //検索ワードの初期値を設定する　変数に前の検索ワードを格納
   var searchLog = "";
-  const settings = {
-    "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
-    "method": "GET",
-  }
+
   $(".search-btn").on("click", function () {
     var searchWord = $("#search-input").val();
     if (searchWord === searchLog) {
@@ -19,6 +16,10 @@ $(function () {
       console.log(searchLog);
     }
     console.log($("#search-input").val());
+    const settings = {
+      "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+      "method": "GET",
+    }
     $.ajax(settings).done(function (response) {
       const result = response['@graph'];
       displayResult(result)
@@ -27,9 +28,14 @@ $(function () {
     });
   });
   function displayResult(data) {
-
+    $(".lists").empty();
+    if (result && result.length > 0) {
+      result.forEach(function (item) {
+        console.log(value);
+      });
+    }
   }
-  function displayError(error) {
+  //function displayError(error) {
 
-  }
+  //}
 });
